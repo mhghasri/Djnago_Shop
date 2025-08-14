e1c58e94813c938ffa99e21ac9a8352dd8ea331d
MHGhari <MHGhasri0021@gmail.com>
2025-08-14 18:43:45 +0330

    complete index {special_sells, articles, all_products}

--- main/views.py
+++ main/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from products.models import Product
+from articles.models import Article
 
 def home_page(request):
+
+    # ----- query ----- #
+    products = Product.objects.all()
+
+    articles = Article.objects.all()
+
+    # ----- special sells ----- #
+
+    only_discounted = products.filter(discount__gt=0)
+
+    new_products = products.order_by('-created_at')
+
+    popular_articles = articles.order_by('-views')
     
-    context = {}
+    context = {
+        'products' : products,
+        'articles' : articles,
+        'only_discounted' : only_discounted,
+        'new_products' : new_products,
+        'popular_articles' : popular_articles,
+    }
     return render(request, 'index.html', context)
